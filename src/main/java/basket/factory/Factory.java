eb51702737ad005ff3d39daab3eefacace0543f3
Marcin M <mircin27@gmail.com>
2021-11-30 19:17:17 +0100

    test

--- src/main/java/basket/factory/Factory.java
+++ src/main/java/basket/factory/Factory.java
@@ -14,6 +14,8 @@ public class Factory {
     int choiceEntry = -1;
     ArrayList<Products> productsArrayList =  new ProductList().makeProductList();
     public Factory() {
+
+
     }
     public void chooseNumberFromMenu(){
         Scanner sc = new Scanner(System.in);
