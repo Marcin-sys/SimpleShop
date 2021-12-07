d34b40cba06915becbd7bc73e6a7f66bf03f28fe
Marcin M <mircin27@gmail.com>
2021-12-07 19:12:22 +0100

    change the method name in Menu changed method to return string

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,12 +1,12 @@
 package menu;
 
 public class Menu {
-    public void menu(){
-        System.out.println("1. Check all product's");
-        System.out.println("2. Add new product to basket");
-        System.out.println("3. Remove product from basket");
-        System.out.println("4. Check what is inside basket");
-        System.out.println("5. Pay for all product inside basket");
-        System.out.println("6. End shopping");
+    public String MenuPrinter(){
+        return "1. Check all product's" +
+                "\n2. Add new product to basket" +
+                "\n3. Remove product from basket" +
+                "\n4. Check what is inside basket" +
+                "\n5. Pay for all product inside basket"+
+                "\n6. End shopping";
     }
 }
