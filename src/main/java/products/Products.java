c49e6536f3701a6754392c9ee5a765bfc6c2df52
Marcin M <mircin27@gmail.com>
2021-12-16 19:33:06 +0100

    Simplified

--- src/main/java/products/Products.java
+++ src/main/java/products/Products.java
@@ -1,25 +1,12 @@
 package products;
 
-public class Products {
-
-    private final int id;
-    private final float price;
-    private final String name;
-
-    public Products(int id, String name, float price) {
-        this.id = id;
-        this.price = price;
-        this.name = name;
-    }
-
-    public record Product(int id, String name, float price) {
-    }
+public record Products(int id, String name, float price) {
 
     public Integer getId() {
         return id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
