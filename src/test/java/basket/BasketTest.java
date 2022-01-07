ac5e68c9ad729c0ffe8460035deb3d126de86ecf
Marcin M <mircin27@gmail.com>
2022-01-07 15:36:19 +0100

    Added testing class

---
+++ src/test/java/basket/BasketTest.java
@@ -0,0 +1,24 @@
+package basket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketTest {
+
+    @Test
+    void addNewItemToBasket() {
+    }
+
+    @Test
+    void removeProductFromBasket() {
+    }
+
+    @Test
+    void showWhatInsideBasket() {
+    }
+
+    @Test
+    void payForBasket() {
+    }
+}
---
+++ src/test/java/basket/factory/ShopManagerTest.java
@@ -0,0 +1,12 @@
+package basket.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopManagerTest {
+
+    @Test
+    void shopManagerMenu() {
+    }
+}
